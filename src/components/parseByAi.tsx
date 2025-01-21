16683ccbd0d9483a8ed35e20c8d2db2504680d4b
topdev126 <que.quao126@gmail.com>
2025-01-21 04:04:09 +0100

    14, 15

--- src/components/parseByAi.tsx
+++ src/components/parseByAi.tsx
@@ -1,7 +1,8 @@
 
 export async function filterResumeItems(rawText: string): Promise<any> {
 
-  const response = await fetch('https://resumebackend-6ywp.onrender.com/api/parse', {
+  // const response = await fetch('https://resumebackend-production-1908.up.railway.app/api/parse', {
+  const response = await fetch('http://localhost:5000/api/parse', {
     method: 'POST',
     headers: {
       'Content-Type': 'text/plain',
